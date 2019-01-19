2dd132ee2ade48362129e071610fec612105842b
Mohammad Amir <m.amir@abfindungsheld.de>
2019-01-19 16:15:03 +0100

    thymelef added

--- src/main/java/com/shah/lab/dao/PatientRepository.java
+++ src/main/java/com/shah/lab/dao/PatientRepository.java
@@ -1,14 +1,12 @@
 package com.shah.lab.dao;
 
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+import com.shah.lab.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.shah.lab.model.User;
 @Repository
-public interface PatientRepository extends CrudRepository<User, String> {
-	@Query("select u from User u where u.name=:name and u.password=:password")
-	User findUserByName(@Param("name")String id,@Param("password")String password);
+public interface PatientRepository extends JpaRepository<User, String> {
+
+	User findByNameAndPassword(String name, String password);
 }
